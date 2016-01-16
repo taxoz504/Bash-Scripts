852d03c56ddc2d78ba072aed16f89d1485dc1cb2
taxoz504 <admin@taxoz.dk>
2016-01-16 11:33:53 +0100

    adding a new script newsite for easy adding new sites to a apache2 server

---
+++ newsite.sh
@@ -0,0 +1,53 @@
+DOMAIN="Site url without HTTP"
+
+echo mkaing folder $DOMAIN
+
+sudo mkdir -p /var/www/$DOMAIN/public_html
+
+sleep 1
+
+echo Making index for $DOMAIN
+
+sleep 1
+
+echo '<html>
+  <head>
+    <title>Welcome</title>
+  </head>
+  <body>
+    <h1>Success! virtual host is working!</h1>
+  </body>
+</html>' > /var/www/$DOMAIN/public_html/index.html
+
+sleep 1
+
+echo Making .conf for $DOMAIN
+
+sleep 1
+
+#ServerAlias only need www in it if you are using a new domain any sub doamin it has to be the same as ServerName
+#
+echo '<VirtualHost *:80>
+        ServerAdmin admin@DOMAIN
+        ServerName DOMAIN
+        ServerAlias www.DOMAIN
+        DocumentRoot /var/www/newsite.taxoz.dk/public_html
+        ErrorLog ${APACHE_LOG_DIR}/error.log
+        CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>' > /etc/apache2/sites-available/$DOMAIN.conf
+
+sleep 1
+
+echo enabling site
+
+sleep 1
+
+sudo a2ensite $DOMAIN.conf
+
+sleep 1
+
+echo Restarting apache2 server
+
+sleep 1
+
+sudo service apache2 restart
